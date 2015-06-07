5bc55af280e3ab1fc388fb4354ea6ee9535a3093
James Fisher <fisher.jc@gmail.com>
2015-06-07 14:13:26 -0500

    WIP solved after sign in route to users#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  authenticated :user do
+    root :to => 'users#show', as: :user_route
+  end
+
   root to: 'welcome#index'
   get 'about' => 'welcome#about'
 
