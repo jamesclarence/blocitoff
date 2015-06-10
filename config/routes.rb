c3cc1e2d0609c860fb15fb5a005856866e456f58
James Fisher <fisher.jc@gmail.com>
2015-06-10 13:37:49 -0500

    routes.rb update for items

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  resources :users do
+    resources :items, only: [:create]
+  end
+
   authenticated :user do
     root :to => 'users#show', as: :user_route
   end
